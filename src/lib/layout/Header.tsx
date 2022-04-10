68f51c94f3f826f859d2a4cc87fbee4a32f8aa78
Hanz <haniel56@zoho.eu>
2022-04-11 01:48:53 +0530

    chore: add navbar

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,19 +1,67 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Stack,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 import Link from "next/link";
 
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex as="header" width="full" align="center">
-      <Heading as="h1" size="md">
-        <Link href="/">nextarter-chakra</Link>
-      </Heading>
+    <Box>
+      <Flex
+        as="header"
+        width="full"
+        align="center"
+        justifyContent="space-between"
+      >
+        <HStack>
+          <Heading as="h1" size="md">
+            <Link href="/">fivehanz</Link>
+          </Heading>
+        </HStack>
 
-      <Box marginLeft="auto">
-        <ThemeToggle />
-      </Box>
-    </Flex>
+        <HStack>
+          <Box display={{ md: "none" }}>
+            <ThemeToggle />
+          </Box>
+          <IconButton
+            size="md"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label="Open Menu"
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </HStack>
+
+        <HStack as="nav" spacing={10} display={{ base: "none", md: "flex" }}>
+          <Link href="/">Home</Link>
+          <Link href="https://blog.fivehanz.xyz">devBlog</Link>
+          {/* <Link href="/blog">Blog</Link> */}
+          <Link href="/contact">Contact</Link>
+          <Box marginLeft="auto">
+            <ThemeToggle />
+          </Box>
+        </HStack>
+      </Flex>
+      {isOpen ? (
+        <Box m={4} display={{ md: "none" }}>
+          <Stack as="nav" spacing={4}>
+            <Link href="/">Home</Link>
+            <Link href="https://blog.fivehanz.xyz">devBlog</Link>
+            {/* <Link href="/blog">Blog</Link> */}
+            <Link href="/contact">Contact</Link>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
 };
 
