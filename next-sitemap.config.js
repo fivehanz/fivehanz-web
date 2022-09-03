0da479618953a32eedf12cc355ac5cdd470b7428
Restyled.io <commits@restyled.io>
2022-09-03 12:20:15 +0000

    Restyled by prettier

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl : "https://fivehanz.xyz",
-  generateRobotsTxt : true,
+  siteUrl: "https://fivehanz.xyz",
+  generateRobotsTxt: true,
 };
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode : true,
-  swcMinify : true,
-  eslint : {
-    dirs : [ "src" ],
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    dirs: ["src"],
   },
 };
 
