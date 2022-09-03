7420ac54757c6af1e925f00e1c593245e5d95218
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-03 17:44:24 +0530

    chore: nextjs from scratch

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://fivehanz.xyz",
+  generateRobotsTxt: true,
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main>
+        <h2>Hey! this is a new Nextjs site.</h2>
+      </main>
+    </>
+  );
+};
+
+export default Home;
