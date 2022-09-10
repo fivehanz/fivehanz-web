e6e79184111f6b3db482914f231e0d01adab7e0a
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-11 04:12:29 +0530

    chore: contact structure

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,12 @@
 const Contact = () => {
   return (
     <section className="contact container" id="contact">
-      Contact
+      <div className="contact-heading">
+        <h2 className="contact-text">contact</h2>
+      </div>
+      <div className="wip">
+        <h2>WIP/TBC</h2>
+      </div>
     </section>
   );
 };
