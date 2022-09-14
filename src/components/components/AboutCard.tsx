84661bc04738e92f92753d6991914e2558bd118f
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-15 02:27:04 +0530

    fix: about card alignment

--- src/components/components/AboutCard.tsx
+++ src/components/components/AboutCard.tsx
@@ -13,10 +13,14 @@ const AboutCard = () => {
         </div>
 
         <p className="paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem velit
-          neque asperiores quam maiores sit voluptate itaque similique sint
-          voluptatibus rem obcaecati, ea nulla nam quaerat doloremque nihil
-          dolores ut.
+          One morning, when Gregor Samsa woke from troubled dreams, he found
+          himself transformed in his bed into a horrible vermin. He lay on his
+          armour-like back, and if he lifted his head a little he could see his
+          brown belly, slightly domed and divided by arches into stiff sections.
+          The bedding was hardly able to cover it and seemed ready to slide off
+          any moment. His many legs, pitifully thin compared with the size of
+          the rest of him, waved about helplessly as he looked.
+          &quot;What&apos;s happened to me?&quot; he thought.
         </p>
 
         <div className="social-icons">
