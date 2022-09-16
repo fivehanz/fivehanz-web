00fdee6429a7e4f0e043132752867b3725888356
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-17 03:43:11 +0530

    fix: refactor var & fn name

--- src/components/components/BurgerMenu.tsx
+++ src/components/components/BurgerMenu.tsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 
 const BurgerMenu = () => {
   const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isBurgerClicked, setIsBurgerClicked] = useState(false);
 
-  const toggleClick = () => {
-    if (!isMenuClicked) {
+  const toggleBurgerClass = () => {
+    if (!isBurgerClicked) {
       setBurgerClass("burger-bar clicked");
     } else {
       setBurgerClass("burger-bar unclicked");
     }
 
-    setIsMenuClicked(!isMenuClicked);
+    setIsBurgerClicked(!isBurgerClicked);
   };
 
   return (
     <div
       className="burger-menu"
       onClick={() => {
-        toggleClick();
+        toggleBurgerClass();
       }}
     >
       <div>
