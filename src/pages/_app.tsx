35cc7aaa56b0e5a34408de9c026c2d5df674ad68
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-09-17 06:57:44 +0530

    feat: toTop button #18

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import GoToTop from "../components/components/GoToTop";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <GoToTop />
+    </>
+  );
 }
 
 export default MyApp;
