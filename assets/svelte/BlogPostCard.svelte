33db2ec0b5c71685c50d5bd2df5f8df21760b684
Hanz <haniel56@zoho.eu>
2023-08-28 20:53:13 +0530

    chore: add blog post card

---
+++ assets/svelte/BlogPostCard.svelte
@@ -0,0 +1,18 @@
+<div class="flex flex-start relative w-72 my-5">
+  <a target="_blank" rel="noreferrer" underline="none" href="#df">
+    <div class="bg-cover bg-center" style="padding-bottom: 60%;" />
+    <div class="flex flex-col justify-between flex-1 p-5">
+      <h2 class="h2">title</h2>
+      <p class="mt-8">creator</p>
+    </div>
+  </a>
+</div>
+
+<style>
+  p {
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    letter-spacing: 0.05em;
+  }
+</style>
