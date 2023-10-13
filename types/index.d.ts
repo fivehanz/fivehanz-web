4998047c57b34de45160fa7a1cdf54a1f2a3cc02
Hanz <haniel56@zoho.eu>
2023-10-13 23:10:47 +0530

    chore: shared types folder

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+type Tag = {
+  id: string;
+  name: string;
+};
+
+type Link = {
+  id: string;
+  title: string;
+  href: string;
+  external?: boolean;
+};
+
+type Project = {
+  id: string;
+  name: string;
+  subtitle: string;
+  description: string;
+  tags: Tag[];
+  links: Link[];
+};
