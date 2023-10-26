53010e0add7f32f4f4d5ed7c48e14dfa25fd59e9
Hanz <haniel56@zoho.eu>
2023-10-26 17:03:00 +0530

    chore: refine init

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+// const { i18n } = require('./next-i18next.config');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
