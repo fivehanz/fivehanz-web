6e6b77354e65384ce524d0277347d3d145d3c3a4
Hanz <haniel56@zoho.eu>
2023-10-26 18:10:48 +0530

    chore: auth0 init

--- apps/admin/components/layout/layout.tsx
+++ apps/admin/components/layout/layout.tsx
@@ -1,10 +1,12 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 /* eslint-disable-next-line */
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
 export function Layout(props: LayoutProps) {
-  return <div>{props.children}</div>;
+  return <UserProvider>{props.children}</UserProvider>;
 }
 
 export default Layout;
